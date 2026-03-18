2e84affe0ecc138268b79e5d186d2659e6291ec9
Tarso Virgilio <tarsovirgilioc@gmail.com>
2026-03-18 17:39:01 -0300

    feat: config NativeWind in project

--- App.tsx
+++ App.tsx
@@ -1,18 +1,23 @@
+import './global.css';
 import { NavigationContainer } from '@react-navigation/native';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from './src/theme/ThemeContext';
 
 import AppNavigator from './src/navigation/AppNavigator';
 import { queryClient } from './src/services/queryClient';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <NavigationContainer>
-          <AppNavigator />
-        </NavigationContainer>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <View className="flex-1 items-center justify-center bg-blue-800">
+      <Text className="text-white text-xl font-bold">Funcionando 🚀</Text>
+    </View>
+    // <QueryClientProvider client={queryClient}>
+    //   <ThemeProvider>
+    //     <NavigationContainer>
+    //       <AppNavigator />
+    //     </NavigationContainer>
+    //   </ThemeProvider>
+    // </QueryClientProvider>
   );
 }
